2ec0c669427c77f50fb557e6e29bee60564b1b8a
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2023-04-17 00:32:07 +0700

    Init project

---
+++ src/main/java/com/sources/nhk/main/SonarJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sources.nhk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarJenkinsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sources/nhk/main/SonarJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sources.nhk.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarJenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
