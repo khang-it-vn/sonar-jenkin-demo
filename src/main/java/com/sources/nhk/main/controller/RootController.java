215307b680542438b9d8ca1d79ab2763e5f2508a
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2023-04-18 00:39:12 +0700

    Update controller

--- src/main/java/com/sources/nhk/main/controller/RootController.java
+++ src/main/java/com/sources/nhk/main/controller/RootController.java
@@ -1,15 +1,16 @@
 package com.sources.nhk.main.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class RootController {
 
     @GetMapping("/")
     public String index()
     {
-        int a,b = Integer.parseInt(null);
 
 
         return "Hello World!!!";
