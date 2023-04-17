374b2c7487a2e0a483c13917b9380e3928ad0eeb
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2023-04-17 23:49:17 +0700

    Update jenkins file

---
+++ src/main/java/com/sources/nhk/main/controller/RootController.java
@@ -0,0 +1,37 @@
+package com.sources.nhk.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String index()
+    {
+        int a,b = Integer.parseInt(null);
+
+
+        return "Hello World!!!";
+    }
+
+
+    @GetMapping("/test2")
+    public String test()
+    {
+        for(int i =0; i <12; i++)
+        {{
+            System.out.println("Xin chao \n");
+            if(i == 1)
+            {
+                System.out.println(1);
+                    if(true)
+                    {
+                        System.out.println("Xin chao duplicate");
+                    }
+            }
+        }}
+
+        return "OK! finished";
+    }
+}
